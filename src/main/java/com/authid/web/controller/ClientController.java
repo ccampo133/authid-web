b96900159f1cc9a02f5e4bf2dbf014048482022e
Chris Campo <ccampo133@users.noreply.github.com>
2016-02-28 15:59:33 -0500

    feat: Add get client by ID method

--- src/main/java/com/authid/web/controller/ClientController.java
+++ src/main/java/com/authid/web/controller/ClientController.java
@@ -2,18 +2,23 @@ package com.authid.web.controller;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 /**
  * @author Chris Campo
  */
-@RestController("/apiw")
+@RestController
+@RequestMapping(path = "/api")
 public class ClientController {
 
     private final JdbcClientDetailsService clientDetailsService;
@@ -28,9 +33,22 @@ public class ClientController {
         return clientDetailsService.listClientDetails();
     }
 
+    @RequestMapping(path = "/clients/{clientId}", method = RequestMethod.GET)
+    public ClientDetails getClientById(@NotNull @PathVariable final String clientId) {
+        return clientDetailsService.loadClientByClientId(clientId);
+    }
+
     @RequestMapping(path = "/clients", method = RequestMethod.POST)
-    public ClientDetails addClientDetails(@NotNull final ClientDetails clientDetails) {
+    public ResponseEntity<ClientDetails> addClientDetails(@NotNull @RequestBody final BaseClientDetails clientDetails) {
         clientDetailsService.addClientDetails(clientDetails);
-        return clientDetailsService.loadClientByClientId(clientDetails.getClientId());
+        final ClientDetails newClientDetails = clientDetailsService.loadClientByClientId(clientDetails.getClientId());
+        // Set the location header to point towards our new resource URI
+        final URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/api/clients")
+                .buildAndExpand(newClientDetails.getClientId())
+                .toUri();
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(location);
+        return new ResponseEntity<>(newClientDetails, headers, HttpStatus.CREATED);
     }
 }
